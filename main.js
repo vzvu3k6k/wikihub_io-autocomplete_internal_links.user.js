abb6a2be645014fe0d9ca945c4b43af81252676c
vzvu3k6k <vzvu3k6k@gmail.com>
2016-07-14 01:05:41 +0900

    Prettify candidate template

--- main.js
+++ main.js
@@ -11,12 +11,39 @@
 
 // Thanks to https://nippo.wikihub.io/@yuta25/20160701125959
 
+function escapeHTML (string) {
+  return string.toString().replace('&', '&amp;')
+    .replace('<', '&lt;')
+    .replace('>', '&gt;')
+    .replace('"', '&quot;')
+    .replace("'", '&#39;')
+}
+
+function HTMLtemplate (strings, ...values) {
+  return strings[0] + strings.slice(1).map((str, index) => {
+    return escapeHTML(values[index]) + str
+  }).join('')
+}
+
+function ymdDate (date) {
+  return `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`
+}
+
 let style = document.createElement('style')
 style.textContent = `
 /* textcomplete関連のスタイルはWikiHub側で指定されている */
-.dropdown-menu.textcomplete-dropdown.ujs-autocomplete-internal-links {
+
+.ujs-autocomplete-internal-links.dropdown-menu.textcomplete-dropdown {
   /* インラインスタイルによってheightが大きく設定されるのを雑に抑制 */
   height: auto !important;
+}
+
+.ujs-autocomplete-internal-links.dropdown-menu .list-group-item {
+  border: none;
+}
+
+.ujs-autocomplete-internal-links.dropdown-menu > li > a {
+  padding: 0;
 }
 `
 document.head.appendChild(style)
@@ -24,20 +51,22 @@ document.head.appendChild(style)
 let jQuery = require('jquery')
 require('jquery-textcomplete')
 
-let _sourceCache, _sourceDirectCache
+let _sourceCache
 function fetchSourcesFromTopPage (rootURL) {
   return _sourceCache || (_sourceCache = new Promise((resolve, reject) => {
     jQuery.ajax({
       url: `${rootURL}articles.atom`,
       dataType: 'xml'
     }).then((doc) => {
-      let sources = Object.create(null)
-      Array.from(doc.querySelectorAll('feed > entry'), (entry) => {
-        let title = entry.querySelector('title').textContent.trim()
-        let url = entry.querySelector('link[rel="alternate"][type="text/html"]').getAttribute('href')
-        sources[title] = url
+      return Array.from(doc.querySelectorAll('feed > entry'), (entry) => {
+        return {
+          author: entry.querySelector('author > name').textContent.trim(),
+          thumbnail: entry.querySelector('thumbnail').getAttribute('url'),
+          title: entry.querySelector('title').textContent.trim(),
+          publishedAt: Date.parse(entry.querySelector('published').textContent.trim()),
+          url: entry.querySelector('link[rel="alternate"][type="text/html"]').getAttribute('href')
+        }
       })
-      return _sourceDirectCache = sources
     }).then(resolve, reject)
   }))
 }
@@ -48,12 +77,23 @@ jQuery('textarea').textcomplete([{
   search: (term, callback) => {
     fetchSourcesFromTopPage(new URL(`https://${location.hostname}/`))
       .then((sources) => {
-        console.log(sources)
-        callback(Object.keys(sources).filter((key) => key.includes(term)))
+        callback(sources.filter((source) => source.title.includes(term)))
       })
   },
-  replace: (title) => {
-    return `[${title}](${_sourceDirectCache[title]})`
+  template: (source) => {
+    return HTMLtemplate`
+<span class="list-group-item media">
+  <div class="pull-left">
+    <img class="avatar-img" title="@${source.author}" src="${source.thumbnail}" width="40" height="40">
+  </div>
+  <div class="media-body">
+    <div class="lgi-heading">${source.title}</div>
+    <div class="lgi-text">Created on ${ymdDate(source.publishedAt)} by ${source.author}</div>
+  </div>
+</span>`
+  },
+  replace: (source) => {
+    return `[${source.title}](${source.url})`
   }
 }], {
   dropdownClassName: 'dropdown-menu textcomplete-dropdown ujs-autocomplete-internal-links'
